61ac7f610f707c4b1a7dd1aa769e7f6a2d08bb98
Ramsensei <ramsensei@estudiantec.cr>
2023-04-28 15:25:01 -0600

    plantillas

--- scr/src/App.jsx
+++ scr/src/App.jsx
@@ -1,10 +1,93 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
+import { Route } from 'wouter'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+
+  // // Plantilla GET
+  // axios.get('http://url')
+  // .then(function (response) {
+  //   console.log(response.data);
+  // })
+  // .catch(function (error) {
+  //   if (error.response) { // GET response with a status code not in range 2xx
+  //     console.log(error.response.data);
+  //     console.log(error.response.status);
+  //     console.log(error.response.headers);
+  //   } else if (error.request) { // no response
+  //     console.log(error.request);
+  //     // instance of XMLHttpRequest in the browser
+  //     // instance ofhttp.ClientRequest in node.js
+  //   } else { // Something wrong in setting up the request
+  //     console.log('Error', error.message);
+  //   }
+  //   console.log(error.config);
+  // });
+
+  // // Plantilla POST
+  // axios.post(
+  //   'http://url',
+  //   {
+  //     //Body
+  //     title: "Title",
+  //     description: "Description",
+  //   },
+  //   {
+  //     headers: {
+  //       "x-access-token": "token-value",
+  //     },
+  //   }
+  // ).then(function (response) {
+  //   console.log(response.data);
+  // }).catch(function (error) {
+  //   if (error.response) { // POST response with a status code not in range 2xx
+  //     console.log(error.response.data);
+  //     console.log(error.response.status);
+  //     console.log(error.response.headers);
+  //   } else if (error.request) { // no response
+  //     console.log(error.request);
+  //     // instance of XMLHttpRequest in the browser
+  //     // instance ofhttp.ClientRequest in node.js
+  //   } else { // Something wrong in setting up the request
+  //     console.log('Error', error.message);
+  //   }
+  //   console.log(error.config);
+  // });
+
+  // // Plantilla PUT
+  // axios.put(
+  //   'http://url',
+  //   {
+  //     //Body
+  //     title: "title",
+  //     description: "Description",
+  //     published: true,
+  //   },
+  //   {
+  //     headers: {
+  //       "x-access-token": "token-value",
+  //     },
+  //   }
+  // ).then(function (response) {
+  //   console.log(response.data);
+  // }).catch(function (error) {
+  //   if (error.response) { // PUT response with a status code not in range 2xx
+  //     console.log(error.response.data);
+  //     console.log(error.response.status);
+  //     console.log(error.response.headers);
+  //   } else if (error.request) { // no response
+  //     console.log(error.request);
+  //     // instance of XMLHttpRequest in the browser
+  //     // instance ofhttp.ClientRequest in node.js
+  //   } else { // Something wrong in setting up the request
+  //     console.log('Error', error.message);
+  //   }
+  //   console.log(error.config);
+  // });
 
   return (
     <>
